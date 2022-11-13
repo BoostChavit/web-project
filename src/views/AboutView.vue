fc7afeae2a5fdd0630893c96d36946eb27c18d5d
unknown <wittawat.pin@ku.th>
2022-11-13 23:30:41 +0700

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,25 @@
 <template>
-  <div v-for="profile in profiles" :key="profile">
-    <div class="">
-      <div>
-        <img :src="profile.img" alt="profile">
+  <body>
+    <link rel="stylesheet" href="src/assets/style.css">
+    <div v-for="profile in profiles" :key="profile" class="inline-block w-1/4 mx-12 my-6">
+    <div>
+      <a :href="profile.img" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div class="flex flex-col justify-center"> <img :src="profile.img" alt="profile" class="rounded-full w-auto h-72 "> </div>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{profile.name}}</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">นาย{{profile.surname}}. </p>
+        <p class="font-normal text-gray-700 dark:text-gray-400">อายุ : {{profile.age}}. ศึกษาอยู่ที่ {{profile.education}}.</p>
+        <p v-if="profile.isStudy" class="font-normal text-gray-700 dark:text-gray-400">สถานะ : กำลังศึกษา.</p>
+        <p v-else-if="profile.isStudy == false" class="font-normal text-gray-700 dark:text-gray-400">สถานะ : ไม่ได้ศึกษาเเล้ว.</p>
+        <div class="mt-4 border-t-4 border-black border-solid">
+          <a :href="profile.facebook"><font-awesome-icon icon="fa-brands fa-facebook" class="mt-3 ml-36 mr-3 text-3xl"/></a>
+          <a :href="profile.ig"><font-awesome-icon icon="fa-brands fa-instagram" class="mr-3 text-3xl"/></a>
+          <a :href="profile.song"><font-awesome-icon icon="fa-solid fa-music" class="text-3xl"/></a>
       </div>
+        
+      </a>
     </div>
   </div>
+  </body>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,29 @@
 <template>
-    
+  <div>
+    <link rel="stylesheet" href="src/assets/style.css">
+  <div class="ml-20 mt-20">
+    <div> 
+      <h1><font-awesome-icon icon="fa-solid fa-location-dot" class="mb-0.5 text-red-500 text-xl"/> <i class="text-xl text-transparent bg-clip-text bg-gradient-to-r from-green-200 via-green-400 to-green-500">KU81 CS36</i> </h1>  
+      <h1><strong class="inline-block text-4xl ml-18 text-center font-mono text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Team Start</strong></h1>
+      <h1><i class="text-white"> เว็บนี้ถูกจัดทำขึ้นโดยมีวัตถุประสงค์เพื่อการเรียนรู้เรื่องการเขียน website จากค่าย PreDevCamp </i></h1>
+      <h1><i class="text-white"> โดยใช้ ภาษา HTML, JavaScript, CSS เเละใช้ framework เป็น vue เพื่อช่วยในการทำงานให้ง่ายยิ่งขึ้น </i></h1>
+      <h1><i class="text-white">สโลแกนของกลุ่มคือ : <q>
+        เรียนวันนี้เเสนเศร้า เเละเหงานัก
+        เหมือนความรักของเราที่เลือนหาย
+        เคยมีคำสัญญาว่าจะรักจนวันตาย
+        เเต่กลับกลายเป็นเหมือน คำอำลา
+      </q> 
+      </i></h1>
+      <a href="https://vuejs.org/"><img class="my-16 ml-10  inline-block hover:scale-105 hover:shadow-black outline-double ; block ; p-6 ; max-w-sm ; bg-white ; rounded-lg ; border ; border-gray-900 ; shadow-md ; hover:bg-gray-500 ; dark:bg-gray-800 ; dark:border-gray-700 ; dark:hover:bg-gray-700" src="../assets/images/logoVue.png" alt="logoVue" width="141"></a>
+      <a href="https://www.w3schools.com/html/"><img class="ml-5  inline-block hover:scale-105 hover:shadow-black outline-double ; block ; p-6 ; max-w-sm ; bg-white ; rounded-lg ; border ; border-gray-900 ; shadow-md ; hover:bg-gray-500 ; dark:bg-gray-800 ; dark:border-gray-700 ; dark:hover:bg-gray-700" src="../assets/images/logoCSS.png" alt="logoHTML" width="128"></a>
+      <a href="https://www.w3schools.com/css/default.asp"><img class="ml-5 inline-block hover:scale-105 hover:shadow-black outline-double ; block ; p-6 ; max-w-sm ; bg-white ; rounded-lg ; border ; border-gray-900 ; shadow-md ; hover:bg-gray-500 ; dark:bg-gray-800 ; dark:border-gray-700 ; dark:hover:bg-gray-700" src="../assets/images/logoHTML.png" alt="logoCSS" width="128"></a>
+      <a href="https://horn-typhoon-d0f.notion.site/Javascript-6d3937143e2741c28ece17df3397de6d"><img class="ml-5 inline-block hover:scale-105 hover:shadow-black outline-double ; block ; p-6 ; max-w-sm ; bg-white ; rounded-lg ; border ; border-gray-900 ; shadow-md ; hover:bg-gray-500 ; dark:bg-gray-800 ; dark:border-gray-700 ; dark:hover:bg-gray-700" src="../assets/images/logoJavascript.png" alt="logoJavaScript" width="128"></a>
+    </div>
+    <div >   
+    </div>
+  </div>
+  </div>
 </template>
-
 <script>
 
 </script>
